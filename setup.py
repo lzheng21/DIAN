1dd20966b6ff7617c33bc7158639142a3e6b608c
lei <lzheng21@uic.edu>
2018-02-22 14:23:43 -0600

    Packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='dian',
+    version='0.1',
+    description='A TensorFlow Library for Implicit Data Recommendation',
+    url='https://github.com/lzheng21/DIAN.git',
+    author='Lei Zheng',
+    author_email='lzheng21@uic.edu',
+    license='MIT',
+    package=['dian'],
+    zip_safe=False
+)
